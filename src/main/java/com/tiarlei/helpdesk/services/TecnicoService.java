e896e4e3d3b5ee01015cc0c4e6183c3afa23db7e
TiarleiNoeremberg <tiarlei.com@gmail.com>
2023-05-29 17:14:22 -0300

    Criado Exceção para objeto não encontrado

--- src/main/java/com/tiarlei/helpdesk/services/TecnicoService.java
+++ src/main/java/com/tiarlei/helpdesk/services/TecnicoService.java
@@ -7,6 +7,7 @@ import org.springframework.stereotype.Service;
 
 import com.tiarlei.helpdesk.domain.Tecnico;
 import com.tiarlei.helpdesk.repositories.TecnicoRepository;
+import com.tiarlei.helpdesk.services.exception.ObjectNotFoundException;
 
 @Service
 public class TecnicoService {
@@ -16,6 +17,7 @@ public class TecnicoService {
 	
 	public Tecnico findById(Integer id) {
 		Optional<Tecnico> obj = repository.findById(id);
-		return obj.orElse(null);	//Caso objeto não encontrado, retorne null.
+		//return obj.orElse(null);	//Caso objeto não encontrado, retorne null.
+		return obj.orElseThrow(() -> new ObjectNotFoundException("Objeto não encontrado! " + id));
 	}
 }
