c18788d393dbeb3b3915ca5241a74c3bc0dd2c98
TiarleiNoeremberg <tiarlei.com@gmail.com>
2023-05-28 21:36:58 -0300

    Anotacoes JPA para classes de domínio

--- src/main/java/com/tiarlei/helpdesk/domain/Cliente.java
+++ src/main/java/com/tiarlei/helpdesk/domain/Cliente.java
@@ -3,16 +3,26 @@ package com.tiarlei.helpdesk.domain;
 import java.util.ArrayList;
 import java.util.List;
 
+import com.tiarlei.helpdesk.domain.enums.Perfil;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.OneToMany;
+
+@Entity
 public class Cliente extends Pessoa {
+	private static final long serialVersionUID = 1L;
 	
+	@OneToMany(mappedBy = "cliente")
 	private List<Chamado> chamados = new ArrayList<>();
 
 	public Cliente() {
 		super();
+		addPerfil(Perfil.CLIENTE);	//toda vez que a classe cliente for instanciada o perfil cliente será adicionado
 	}
 
 	public Cliente(Integer id, String nome, String cpf, String email, String senha) {
 		super(id, nome, cpf, email, senha);
+		addPerfil(Perfil.CLIENTE);	//toda vez que a classe cliente for instanciada o perfil cliente será adicionado
 	}
 
 	public List<Chamado> getChamados() {
--- src/main/java/com/tiarlei/helpdesk/domain/Tecnico.java
+++ src/main/java/com/tiarlei/helpdesk/domain/Tecnico.java
@@ -3,16 +3,26 @@ package com.tiarlei.helpdesk.domain;
 import java.util.ArrayList;
 import java.util.List;
 
-public class Tecnico extends Pessoa {
+import com.tiarlei.helpdesk.domain.enums.Perfil;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.OneToMany;
+
+@Entity
+public class Tecnico extends Pessoa {
+	private static final long serialVersionUID = 1L;
+	
+	@OneToMany(mappedBy = "tecnico")
 	private List<Chamado> chamados = new ArrayList<>();
 
 	public Tecnico() {
 		super();
+		addPerfil(Perfil.TECNICO);	//toda vez que a classe cliente for instanciada o perfil tecnico será adicionado
 	}
 
 	public Tecnico(Integer id, String nome, String cpf, String email, String senha) {
 		super(id, nome, cpf, email, senha);
+		addPerfil(Perfil.TECNICO);	//toda vez que a classe cliente for instanciada o perfil tecnico será adicionado
 	}
 
 	public List<Chamado> getChamados() {
