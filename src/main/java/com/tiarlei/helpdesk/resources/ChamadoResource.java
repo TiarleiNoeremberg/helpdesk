186d1b3d81761dd92723fa00f19ab60fd2bc5806
TiarleiNoeremberg <tiarlei.com@gmail.com>
2023-05-30 23:12:07 -0300

    Create para Chamado

--- src/main/java/com/tiarlei/helpdesk/resources/ChamadoResource.java
+++ src/main/java/com/tiarlei/helpdesk/resources/ChamadoResource.java
@@ -1,5 +1,6 @@
 package com.tiarlei.helpdesk.resources;
 
+import java.net.URI;
 import java.util.List;
 import java.util.stream.Collectors;
 
@@ -7,12 +8,17 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import com.tiarlei.helpdesk.domain.Chamado;
 import com.tiarlei.helpdesk.domain.dtos.ChamadoDTO;
 import com.tiarlei.helpdesk.services.ChamadoService;
+
+import jakarta.validation.Valid;
 
 @RestController
 @RequestMapping(value = "/chamados")
@@ -32,5 +38,12 @@ public class ChamadoResource {
 		List<Chamado> list = service.findAll();
 		List<ChamadoDTO> listDTO = list.stream().map(obj -> new ChamadoDTO(obj)).collect(Collectors.toList());	//Converte lista Chamado para lista ChamadoDTO
 		return ResponseEntity.ok().body(listDTO);
+	}
+	
+	@PostMapping
+	public ResponseEntity<ChamadoDTO> create(@Valid @RequestBody ChamadoDTO objDTO){
+		Chamado Obj = service.create(objDTO);
+		URI uri = ServletUriComponentsBuilder.fromCurrentRequestUri().path("/{id}").buildAndExpand(Obj.getId()).toUri();
+		return ResponseEntity.created(uri).build();
 	}
 }
