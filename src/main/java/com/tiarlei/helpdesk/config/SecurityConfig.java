fc2aeacd922e2c4df71ac600941c1a10927568c0
TiarleiNoeremberg <tiarlei.com@gmail.com>
2023-06-05 19:19:17 -0300

    Criado filtro de autenticacao JWTAuthenticationFilter

--- src/main/java/com/tiarlei/helpdesk/config/SecurityConfig.java
+++ src/main/java/com/tiarlei/helpdesk/config/SecurityConfig.java
@@ -2,18 +2,22 @@ package com.tiarlei.helpdesk.config;
 
 import java.util.Arrays;
 
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.core.env.Environment;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.web.cors.CorsConfiguration;
 import org.springframework.web.cors.CorsConfigurationSource;
 import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+
+import com.tiarlei.helpdesk.security.JWTAuthenticationFilter;
+import com.tiarlei.helpdesk.security.JWTUtil;
 
 @EnableWebSecurity
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
@@ -22,6 +26,10 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 	
 	@Autowired
 	private Environment env;
+	@Autowired
+	private JWTUtil jwtUtil;
+	@Autowired
+	private UserDetailsService  userDetailsService;
 
 	@Override
 	protected void configure(HttpSecurity http) throws Exception {
@@ -30,6 +38,7 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 		}
 		
 		http.cors().and().csrf().disable();
+		http.addFilter(new JWTAuthenticationFilter(authenticationManager(), jwtUtil));
 		
 		http.authorizeRequests()
 		.antMatchers(PUBLIC_MATCHES)
@@ -37,6 +46,11 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 		.authenticated();
 		
 		http.sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS);
+	}
+	
+	@Override
+	protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+		auth.userDetailsService(userDetailsService).passwordEncoder(bCryptPasswordEncoder());
 	}
 	
 	@Bean
